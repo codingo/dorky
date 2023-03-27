efa452281560e752b30091e2034e9d95e3bba603
Michael Skelton <codingo@protonmail.com>
2023-03-27 23:21:24 +1000

    Update main.go

--- main.go
+++ main.go
@@ -7,7 +7,6 @@ import (
 	"flag"
 	"fmt"
 	"net/http"
-	"net/url"
 	"os"
 	"regexp"
 	"strings"
@@ -18,60 +17,118 @@ import (
 	"golang.org/x/time/rate"
 )
 
+type config struct {
+	oFlag        bool
+	rFlag        bool
+	uFlag        bool
+	maxFlag      int
+	cFlag        bool
+	ghFlag       bool
+	glFlag       bool
+	sFlag        bool
+	vFlag        bool
+}
+
 var (
-	oFlag     = flag.Bool("o", false, "search for organization names")
-	rFlag     = flag.Bool("r", false, "search for repository names")
-	uFlag     = flag.Bool("u", false, "search for username matches")
-	maxFlag   = flag.Int("max", 10, "maximum search results per category")
-	cFlag     = flag.Bool("c", false, "clean input URLs")
-	ghFlag    = flag.Bool("gh", false, "search only GitHub")
-	glFlag    = flag.Bool("gl", false, "search only GitLab")
-	sFlag     = flag.Bool("s", false, "simple output style for piping to another tool")
-	urlRegexp = regexp.MustCompile(`^https?://(?:www\.)?([^/]+)`)
+	flags = config{}
+	urlRegexp    = regexp.MustCompile(`^https?://(?:www\.)?([^/]+)`)
+	spaceRegexp  = regexp.MustCompile(`\s+`)
+	wordPatterns = []string{"", "-", ""}
 )
+
+func init() {
+	flag.BoolVar(&flags.oFlag, "o", false, "search for organization names")
+	flag.BoolVar(&flags.rFlag, "r", false, "search for repository names")
+	flag.BoolVar(&flags.uFlag, "u", false, "search for username matches")
+	flag.IntVar(&flags.maxFlag, "max", 10, "maximum search results per category")
+	flag.BoolVar(&flags.cFlag, "c", false, "clean input URLs")
+	flag.BoolVar(&flags.ghFlag, "gh", false, "search only GitHub")
+	flag.BoolVar(&flags.glFlag, "gl", false, "search only GitLab")
+	flag.BoolVar(&flags.sFlag, "s", false, "simple output style for piping to another tool")
+	flag.BoolVar(&flags.vFlag, "v", false, "enable verbose mode")
+}
 
 func main() {
 	flag.Parse()
+	validateFlags(flags)
 
-	// Ensure at least one search flag is specified
-	if !(*oFlag || *rFlag || *uFlag) {
+	verbosePrint("Reading and cleaning words...\n")
+	words := readAndCleanWords(flags)
+	verbosePrint("Words cleaned.\n")
+
+	verbosePrint("Searching platforms...\n")
+	searchPlatforms(words, flags)
+	verbosePrint("Platform search completed.\n")
+}
+
+func validateFlags(cfg config) {
+	if !(cfg.oFlag || cfg.rFlag || cfg.uFlag) {
 		fmt.Println("At least one search flag (-o, -r, or -u) must be specified")
 		os.Exit(1)
 	}
+	verbosePrint("Flags validated.\n")
+}
 
-	// Read input words from stdin and store them in a map to avoid duplicates
+func verbosePrint(format string, a ...interface{}) {
+	if flags.vFlag {
+		fmt.Printf(format, a...)
+	}
+}
+
+func readAndCleanWords(cfg config) map[string]struct{} {
 	words := make(map[string]struct{})
 	scanner := bufio.NewScanner(os.Stdin)
+
 	for scanner.Scan() {
 		word := strings.TrimSpace(scanner.Text())
-		if *cFlag {
-			word = cleanWord(word)
-		}
+		processWord(word, words, cfg)
+	}
+	checkScannerError(scanner)
 
-		if _, exists := words[word]; !exists {
-			words[word] = struct{}{}
-		}
+	return words
+}
+
+func processWord(word string, words map[string]struct{}, cfg config) {
+	if cfg.cFlag {
+		word = cleanWord(word)
 	}
 
-	// Check for errors while reading stdin
+	addWordToMap(words, word)
+	word = removeWhitespace(word)
+	wordLines := strings.Split(word, "\n")
+
+	for _, w := range wordLines {
+		addWordToMap(words, w)
+	}
+}
+
+func addWordToMap(words map[string]struct{}, word string) {
+	if _, exists := words[word]; !exists {
+		words[word] = struct{}{}
+	}
+}
+
+func checkScannerError(scanner *bufio.Scanner) {
 	if err := scanner.Err(); err != nil {
 		fmt.Printf("Error reading stdin: %s\n", err)
 		os.Exit(1)
 	}
+}
 
-	// Perform searches on GitHub and/or GitLab for each input word
+func searchPlatforms(words map[string]struct{}, cfg config) {
 	for word := range words {
-		if !*glFlag {
-			searchGitHub(word)
+		verbosePrint("Searching GitHub for word: %s\n", word)
+		if !cfg.glFlag {
+			searchGitHub(word, cfg)
 		}
 
-		if !*ghFlag {
-			searchGitLab(word)
+		verbosePrint("Searching GitLab for word: %s\n", word)
+		if !cfg.ghFlag {
+			searchGitLab(word, cfg)
 		}
 	}
 }
 
-// cleanWord removes the URL scheme and www. prefix from the input word
 func cleanWord(word string) string {
 	match := urlRegexp.FindStringSubmatch(word)
 	if len(match) > 1 {
@@ -80,33 +137,36 @@ func cleanWord(word string) string {
 	return word
 }
 
-// searchGitHub performs searches on GitHub based on the specified flags
-func searchGitHub(query string) {
-	if *oFlag {
-		searchGitHubOrganizations(query, *maxFlag)
+func removeWhitespace(word string) string {
+	removedSpaces := spaceRegexp.ReplaceAllString(word, "")
+	withHyphens := spaceRegexp.ReplaceAllString(word, "-")
+	return removedSpaces + "\n" + withHyphens
+}
+
+func searchGitHub(query string, cfg config) {
+	if cfg.oFlag {
+		searchGitHubOrganizations(query, cfg.maxFlag)
 	}
 
-	if *rFlag {
-		searchGitHubRepositories(query, *maxFlag)
+	if cfg.rFlag {
+		searchGitHubRepositories(query, cfg.maxFlag)
 	}
 
-	if *uFlag {
-		searchGitHubUsers(query, *maxFlag)
+	if cfg.uFlag {
+		searchGitHubUsers(query, cfg.maxFlag)
 	}
 }
 
-// searchGitLab performs searches on GitLab based on the specified flags
-func searchGitLab(query string) {
-	if *oFlag || *uFlag {
-		searchGitLabGroupsAndUsers(query, *maxFlag)
+func searchGitLab(query string, cfg config) {
+	if cfg.oFlag || cfg.uFlag {
+		searchGitLabGroupsAndUsers(query, cfg.maxFlag)
 	}
 
-	if *rFlag {
-		searchGitLabProjects(query, *maxFlag)
+	if cfg.rFlag {
+		searchGitLabProjects(query, cfg.maxFlag)
 	}
 }
 
-// searchGitHubOrganizations searches for organizations on GitHub with the given query
 func searchGitHubOrganizations(query string, maxResults int) {
 	ctx := context.Background()
 	client, err := createGithubClient(ctx)
@@ -116,21 +176,20 @@ func searchGitHubOrganizations(query string, maxResults int) {
 	}
 
 	opt := &github.SearchOptions{ListOptions: github.ListOptions{PerPage: maxResults}}
-	results, _, err := client.Search.Organizations(ctx, query, opt)
+	results, _, err := client.Search.Users(ctx, "type:org "+query, opt)
 	if err != nil {
 		fmt.Printf("Error searching organizations: %s\n", err)
 		return
 	}
 
-	orgLogins := make([]string, len(results.Organizations))
-	for i, org := range results.Organizations {
+	orgLogins := make([]string, len(results.Users))
+	for i, org := range results.Users {
 		orgLogins[i] = *org.Login
 	}
 
 	printResults(fmt.Sprintf("GitHub organizations matching '%s'", query), orgLogins)
 }
 
-// searchGitHubRepositories searches for repositories on GitHub with the given query
 func searchGitHubRepositories(query string, maxResults int) {
 	ctx := context.Background()
 	client, err := createGithubClient(ctx)
@@ -154,7 +213,6 @@ func searchGitHubRepositories(query string, maxResults int) {
 	printResults(fmt.Sprintf("GitHub repositories matching '%s'", query), repoNames)
 }
 
-// searchGitHubUsers searches for users on GitHub with the given query
 func searchGitHubUsers(query string, maxResults int) {
 	ctx := context.Background()
 	client, err := createGithubClient(ctx)
@@ -164,7 +222,7 @@ func searchGitHubUsers(query string, maxResults int) {
 	}
 
 	opt := &github.SearchOptions{ListOptions: github.ListOptions{PerPage: maxResults}}
-	results, _, err := client.Search.Users(ctx, query, opt)
+	results, _, err := client.Search.Users(ctx, "type:user "+query, opt)
 	if err != nil {
 		fmt.Printf("Error searching users: %s\n", err)
 		return
@@ -178,7 +236,6 @@ func searchGitHubUsers(query string, maxResults int) {
 	printResults(fmt.Sprintf("GitHub users matching '%s'", query), userLogins)
 }
 
-// createGithubClient creates a new GitHub client with the given context and access token from the environment variable
 func createGithubClient(ctx context.Context) (*github.Client, error) {
 	token := os.Getenv("GITHUB_ACCESS_TOKEN")
 	if token == "" {
@@ -189,17 +246,16 @@ func createGithubClient(ctx context.Context) (*github.Client, error) {
 		&oauth2.Token{AccessToken: token},
 	)
 	tc := oauth2.NewClient(ctx, ts)
-
-	client := github.NewClient(tc)
-	client.Client.Transport = &rateLimitedTransport{
+	tc.Transport = &rateLimitedTransport{
 		transport: tc.Transport,
 		limiter:   rate.NewLimiter(rate.Every(10), 10),
 	}
 
+	client := github.NewClient(tc)
+
 	return client, nil
 }
 
-// rateLimitedTransport is a custom transport that adds rate limiting to the GitHub API requests
 type rateLimitedTransport struct {
 	transport http.RoundTripper
 	limiter   *rate.Limiter
@@ -213,7 +269,6 @@ func (t *rateLimitedTransport) RoundTrip(req *http.Request) (*http.Response, err
 	return t.transport.RoundTrip(req)
 }
 
-// searchGitLabGroupsAndUsers searches for groups and/or users on GitLab with the given query
 func searchGitLabGroupsAndUsers(query string, maxResults int) {
 	client, err := createGitLabClient()
 	if err != nil {
@@ -221,14 +276,14 @@ func searchGitLabGroupsAndUsers(query string, maxResults int) {
 		return
 	}
 
-	opt := &gitlab.SearchOptions{PerPage: maxResults}
-	groups, _, err := client.Search.GroupSearch(query, opt)
+	opt := &gitlab.ListGroupsOptions{Search: gitlab.String(query), ListOptions: gitlab.ListOptions{PerPage: maxResults}}
+	groups, _, err := client.Groups.ListGroups(opt)
 	if err != nil {
 		fmt.Printf("Error searching GitLab groups: %s\n", err)
 		return
 	}
 
-	if *oFlag {
+	if flags.oFlag {
 		groupFullPaths := make([]string, len(groups))
 		for i, group := range groups {
 			groupFullPaths[i] = group.FullPath
@@ -237,13 +292,13 @@ func searchGitLabGroupsAndUsers(query string, maxResults int) {
 		printResults(fmt.Sprintf("GitLab groups matching '%s'", query), groupFullPaths)
 	}
 
-	users, _, err := client.Search.UserSearch(query, opt)
+	users, _, err := client.Users.ListUsers(&gitlab.ListUsersOptions{Search: gitlab.String(query), ListOptions: gitlab.ListOptions{PerPage: maxResults}})
 	if err != nil {
 		fmt.Printf("Error searching GitLab users: %s\n", err)
 		return
 	}
 
-	if *uFlag {
+	if flags.uFlag {
 		userUsernames := make([]string, len(users))
 		for i, user := range users {
 			userUsernames[i] = user.Username
@@ -253,7 +308,6 @@ func searchGitLabGroupsAndUsers(query string, maxResults int) {
 	}
 }
 
-// searchGitLabProjects searches for projects on GitLab with the given query
 func searchGitLabProjects(query string, maxResults int) {
 	client, err := createGitLabClient()
 	if err != nil {
@@ -261,8 +315,8 @@ func searchGitLabProjects(query string, maxResults int) {
 		return
 	}
 
-	opt := &gitlab.SearchOptions{PerPage: maxResults}
-	projects, _, err := client.Search.ProjectSearch(query, opt)
+	opt := &gitlab.ListProjectsOptions{Search: gitlab.String(query), ListOptions: gitlab.ListOptions{PerPage: maxResults}}
+	projects, _, err := client.Projects.ListProjects(opt)
 	if err != nil {
 		fmt.Printf("Error searching GitLab projects: %s\n", err)
 		return
@@ -276,7 +330,6 @@ func searchGitLabProjects(query string, maxResults int) {
 	printResults(fmt.Sprintf("GitLab projects matching '%s'", query), projectFullPaths)
 }
 
-// createGitLabClient creates a new GitLab client with the access token from the environment variable
 func createGitLabClient() (*gitlab.Client, error) {
 	token := os.Getenv("GITLAB_ACCESS_TOKEN")
 	if token == "" {
@@ -291,9 +344,8 @@ func createGitLabClient() (*gitlab.Client, error) {
 	return client, nil
 }
 
-// printResults displays the search results with a header or in a simple format based on the sFlag
 func printResults(header string, results []string) {
-	if *sFlag {
+	if flags.sFlag {
 		for _, result := range results {
 			fmt.Println(result)
 		}
